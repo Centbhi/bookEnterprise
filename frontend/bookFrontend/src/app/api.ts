a44af945ec36a8a66a668ab36e6828ad18a6425e
Centhor <vincenthizon266@gmail.com>
2025-10-03 23:54:00 +0800

    api defs

--- frontend/bookFrontend/src/app/api.ts
+++ frontend/bookFrontend/src/app/api.ts
@@ -2,14 +2,46 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Book {
+  id: number;
+  title: string;
+  genre: string;
+  status: string;
+  datePublished: string;
+  author: string;
+  publisher: string;
+  description: string;
+  coverImgUrl: string;
+
+  quantityStock: number;
+  price: number;
+  rating: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 
-export class ApiService {
-  constructor(private http: HttpClient) {}
+export class ApiService{
+  constructor(private http: HttpClient){}
 
-  getHello(): Observable<{ msg: string }> {
-    return this.http.get<{ msg: string }>('/api/hello');
+  getBooks() : Observable<Book[]> {
+    return this.http.get<Book[]>(`/api/book`);
+  }
+
+  getBook(id:number) : Observable<Book> {
+    return this.http.get<Book>(`/api/book/${id}`);
+  }
+
+  createBook(book:Book) : Observable<Book> {
+    return this.http.post<Book>(`/api/book`,book);
+  }
+
+  updateBook(id:number,book:Book) : Observable<Book> {
+    return this.http.put<Book>(`/api/book/{id}`,book);
+  }
+
+  deleteBook(id:number) : Observable<void>{
+    return this.http.delete<void>(`/api/book/${id}`);
   }
 }
